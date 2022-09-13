ac1ce95cbfe9ad79492df9428b0119b652188946
Imran <aakhtarimran92@gmail.com>
2022-09-13 20:45:25 +0530

    added logo and navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import SearchBar from "./SearchBar";
+
+const Navbar = () => {
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        postion: "sticky",
+        background: "#000",
+        top: 0,
+        justifyContent: "space-between",
+      }}
+    >
+      <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+        <img
+          src={require("../assets/logo.png")}
+          alt="Youtube Clone Logo"
+          height={45}
+        />
+        <SearchBar />
+      </Link>
+    </Stack>
+  );
+};
+
+export default Navbar;
